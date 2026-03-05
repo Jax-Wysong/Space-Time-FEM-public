4277899a10c58282c0e35a19aeb6f78db63c2374
jgwysong@jacks.local <jgwysong@jacks.local@cllogin002.cm.cluster>
2026-03-04 21:20:07 -0600

    removing from pub directory

--- numerical_relativity/gravitational_radiation_non-rotating_black_hole/include/jacobian.h
+++
@@ -1,8 +0,0 @@
-#ifndef JACOBIAN_H
-#define JACOBIAN_H
-
-#include "appctx.h"
-
-PetscErrorCode FormJacobian(SNES snes, Vec U, Mat J, Mat P, void *ctx);
-
-#endif /* JACOBIAN_H */
--- numerical_relativity/gravitational_radiation_non-rotating_black_hole/include/residual.h
+++
@@ -1,8 +0,0 @@
-#ifndef RESIDUAL_H
-#define RESIDUAL_H
-
-#include "appctx.h"
-
-PetscErrorCode FormResidual(SNES snes, Vec U, Vec R, void *ctx);
-
-#endif /* RESIDUAL_H */
